0afe55a279902c40308c8bbc2ef533e1ab2cb35a
Talha Kum <orhantalhakum@gmail.com>
2021-05-15 11:03:13 +0300

    Initial commit
    Initial commit

---
+++ src/test/java/com/talhakum/linkconverter/LinkConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talhakum.linkconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
